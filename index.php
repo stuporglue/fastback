f53169627c4a84c7708784ad8269d2989e545e19
Michael Moore <stuporglue@gmail.com>
2022-02-15 00:57:44 -0600

    jqui link fix

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ class FastbackOutput {
 			<link rel="shortcut icon" href="fastback_assets/favicon.png"> 
 			<link rel="apple-touch-icon" href="fastback_assets/favicon.png">
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-			<link rel="stylesheet" href="fastback_assets/jquery-ui-1.12.1/jquery-ui.min.css">
+			<link rel="stylesheet" href="fastback_assets/jquery-ui.min.css">
 			<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
 			<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.0.3/dist/MarkerCluster.Default.css"/>
 			<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css"/>
