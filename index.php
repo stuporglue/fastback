3a554c1295189f7c5eb6aaafca475b975a175193
Michael Moore <stuporglue@gmail.com>
2023-05-07 23:31:55 +0200

    New way to handle getting exif info, avoids breaks in fgets info lines

--- index.php
+++ index.php
@@ -147,46 +147,46 @@ class FastbackOutput {
 		$html .= '</div>';
 		$html .= '<input id="speedslide" type="range" orient="vertical" min="0" max="100" value="0"/>';
 		$html .= '<div id="resizer">';
-			$html .= '<input type="range" min="1" max="10" value="5" class="slider" id="zoom">';
-			$html .= '<div id="globeicon"></div>';
-			$html .= '<div id="rewindicon"></div>';
-			$html .= '<div id="calendaricon"><input readonly id="datepicker" type="text"></div>';
+		$html .= '<input type="range" min="1" max="10" value="5" class="slider" id="zoom">';
+		$html .= '<div id="globeicon"></div>';
+		$html .= '<div id="rewindicon"></div>';
+		$html .= '<div id="calendaricon"><input readonly id="datepicker" type="text"></div>';
 		$html .= '</div>';
 		$html .= '<div id="thumb">
-					<div id="thumbcontent"></div>
-					<div id="thumbleft" class="thumbctrl">LEFT</div>
-					<div id="thumbright" class="thumbctrl">RIGHT</div>
-					<div id="thumbcontrols">
-						<div id="thumbclose">🆇</div>
-						<div id="thumbdownload"></div>
-						<div id="thumbflag"></div>
-						<div id="thumbinfo"></div>
-						<div id="socialbuttons">
-							<!-- Sharingbutton Facebook -->
-							<a id="share_fb" class="resp-sharing-button__link" href="about:blank" target="_blank" rel="noopener" aria-label="">
-							<div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/></svg>
-								</div>
-							</div>
-							</a>
+			<div id="thumbcontent"></div>
+			<div id="thumbleft" class="thumbctrl">LEFT</div>
+			<div id="thumbright" class="thumbctrl">RIGHT</div>
+			<div id="thumbcontrols">
+			<div id="thumbclose">🆇</div>
+			<div id="thumbdownload"></div>
+			<div id="thumbflag"></div>
+			<div id="thumbinfo"></div>
+			<div id="socialbuttons">
+			<!-- Sharingbutton Facebook -->
+			<a id="share_fb" class="resp-sharing-button__link" href="about:blank" target="_blank" rel="noopener" aria-label="">
+			<div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/></svg>
+			</div>
+			</div>
+			</a>
 
-							<!-- Sharingbutton E-Mail -->
-							<a id="share_email" class="resp-sharing-button__link" href="about:blank" target="_blank" rel="noopener" aria-label="">
-							<div class="resp-sharing-button resp-sharing-button--email resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22 4H2C.9 4 0 4.9 0 6v12c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.25 14.43l-3.5 2c-.08.05-.17.07-.25.07-.17 0-.34-.1-.43-.25-.14-.24-.06-.55.18-.68l3.5-2c.24-.14.55-.06.68.18.14.24.06.55-.18.68zm4.75.07c-.1 0-.2-.03-.27-.08l-8.5-5.5c-.23-.15-.3-.46-.15-.7.15-.22.46-.3.7-.14L12 13.4l8.23-5.32c.23-.15.54-.08.7.15.14.23.07.54-.16.7l-8.5 5.5c-.08.04-.17.07-.27.07zm8.93 1.75c-.1.16-.26.25-.43.25-.08 0-.17-.02-.25-.07l-3.5-2c-.24-.13-.32-.44-.18-.68s.44-.32.68-.18l3.5 2c.24.13.32.44.18.68z"/></svg>
-								</div>
-							</div>
-							</a>
+			<!-- Sharingbutton E-Mail -->
+			<a id="share_email" class="resp-sharing-button__link" href="about:blank" target="_blank" rel="noopener" aria-label="">
+			<div class="resp-sharing-button resp-sharing-button--email resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22 4H2C.9 4 0 4.9 0 6v12c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.25 14.43l-3.5 2c-.08.05-.17.07-.25.07-.17 0-.34-.1-.43-.25-.14-.24-.06-.55.18-.68l3.5-2c.24-.14.55-.06.68.18.14.24.06.55-.18.68zm4.75.07c-.1 0-.2-.03-.27-.08l-8.5-5.5c-.23-.15-.3-.46-.15-.7.15-.22.46-.3.7-.14L12 13.4l8.23-5.32c.23-.15.54-.08.7.15.14.23.07.54-.16.7l-8.5 5.5c-.08.04-.17.07-.27.07zm8.93 1.75c-.1.16-.26.25-.43.25-.08 0-.17-.02-.25-.07l-3.5-2c-.24-.13-.32-.44-.18-.68s.44-.32.68-.18l3.5 2c.24.13.32.44.18.68z"/></svg>
+			</div>
+			</div>
+			</a>
 
-							<!-- Sharingbutton WhatsApp -->
-							<a id="share_whatsapp" class="resp-sharing-button__link" href="about:blank" target="_blank" rel="noopener" aria-label="">
-							<div class="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z"/></svg>
-								</div>
-							</div>
-							</a>
-						</div>
-					</div>';
+			<!-- Sharingbutton WhatsApp -->
+			<a id="share_whatsapp" class="resp-sharing-button__link" href="about:blank" target="_blank" rel="noopener" aria-label="">
+			<div class="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z"/></svg>
+			</div>
+			</div>
+			</a>
+			</div>
+			</div>';
 		$html .= '</div>';
 		$html .= '<script src="fastback_assets/jquery.min.js"></script>';
 		$html .= '<script src="fastback_assets/jquery-ui.min.js"></script>';
@@ -201,7 +201,7 @@ class FastbackOutput {
 			var FastbackBase = "' . $_SERVER['SCRIPT_NAME'] . '";
 		var FastbackBase = "' . $_SERVER['SCRIPT_NAME'] . '";
 		var fastback = new Fastback({
-			cacheurl:    "' . $this->cacheurl . '",
+		cacheurl:    "' . $this->cacheurl . '",
 			photourl:    "' . $this->photourl .'",
 			fastbackurl: "' . $_SERVER['SCRIPT_NAME'] . '"
 	});
@@ -359,6 +359,10 @@ class FastbackOutput {
 					$this->log("Debug enabled");
 				}
 			} 
+
+			if ( $this->debug ) {
+				$this->nproc = 1;
+			}
 
 			$this->log("Using $this->nproc processes for forks");
 			$this->log("Using sqlite database " . $this->sqlitefile);
@@ -965,10 +969,38 @@ class FastbackOutput {
 				$cur_exif = array();
 				$end_of_exif = FALSE;
 
+				$exifout = "";
 				while(!$end_of_exif){
-					// Handle stdout
+					// Handle stdout. Build a single chunk of text then split it since some tag output seems to get split in some cases
 					$line = fgets($pipes[1]);	
-					if ($line !== FALSE ) { 
+					if ($line == FALSE ) {
+						continue;
+					}
+
+					$exifout .= $line;
+
+					if (preg_match('/.*{ready}$/',$line)){
+						$end_of_exif = TRUE;
+
+						$exifout = preg_replace('/\n\s*{ready}\s*/','',$exifout);
+					}
+
+					// Handle stderr
+					$err = fgets($pipes[2]);
+					if ( $err !== FALSE ) {
+						$no_err = FALSE;
+						$this->log($err);
+					}
+
+					if ($err === FALSE && $line === FALSE) {
+						time_nanosleep(0,200);
+					}
+				}
+
+				$exifout = explode("\n",$exifout);
+
+				foreach($exifout as $line) {
+
 						$line = trim($line);
 
 						if ( preg_match('/^======== (.*)/',$line, $matches ) ) {
@@ -999,24 +1031,14 @@ class FastbackOutput {
 							// do nothing
 						} else {
 							$this->log("Don't know how to handle exif line '" . $line . "'");
-							die("Quitting for fun");
+							// die("Quitting for fun");
 						}
 					}
 
-					// Handle stderr
-					$err = fgets($pipes[2]);
-					if ( $err !== FALSE ) {
-						$no_err = FALSE;
-						$this->log($err);
-					}
 
-					if ($err === FALSE && $line === FALSE) {
-						time_nanosleep(0,200);
-					}
+					$cur_exif = array_filter($cur_exif);
+					$found_exif[$file] = json_encode($cur_exif,JSON_FORCE_OBJECT);
 				}
-
-				$found_exif[$file] = json_encode($cur_exif,JSON_FORCE_OBJECT);
-			}
 
 			$this->update_case_when("UPDATE fastback SET _util=NULL, exif=CASE",$found_exif,"ELSE exif END",True);
 
